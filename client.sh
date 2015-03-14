984e2074b957ef6ea8d94c3c090d459bcf13ad6f
Colin Chan <colinchan@lumeh.org>
2015-03-14 00:06:43 -0400

    Finish client

--- client.sh
+++ client.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-name=colin
+[[ $# -ne 2 ]] && echo "usage: client.sh NAME THEME" && exit 1
+played=False
 while true; do
-    sleep 5
-    ishome=$(curl "http://fence.lumeh.org/$name/ishome")
-    if [ $ishome == True ]; then
-        mplayer -q
+    sleep 1
+    ishome=$(curl -s "http://fence.lumeh.org/$1/ishome")
+    if [[ $ishome == True && $played == False ]]; then
+        mplayer -really-quiet "$2" >/dev/null 2>&1
+        played=True
     fi
+    [[ $ishome == False ]] && played=False
 done
