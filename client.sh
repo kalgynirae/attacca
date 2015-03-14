5331f16ae39797be0b242106f623c5c97d4d4303
Colin Chan <colinchan@lumeh.org>
2015-03-14 04:13:26 -0400

    Make the client more user-friendly

--- client.sh
+++ client.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-[[ $# -ne 2 ]] && echo "usage: client.sh NAME THEME" && exit 1
-played=False
+[[ $# -ne 3 ]] && echo "usage: client.sh SERVER NAME THEME" && exit 1
 while true; do
     sleep 1
-    ishome=$(curl -s "http://fence.lumeh.org/$1/ishome")
+    ishome=$(curl -s "http://$1/$2/ishome")
     if [[ $ishome == True && $played == False ]]; then
-        mplayer -really-quiet "$2" >/dev/null 2>&1
+        echo "Welcome back, ${2^}!"
+        mplayer -really-quiet "$3" >/dev/null 2>&1
         played=True
     fi
     [[ $ishome == False ]] && played=False
